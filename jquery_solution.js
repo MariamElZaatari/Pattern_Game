5b671bff1f44ab4bd4daf8fe6b604d64794cbede
MariamElZaatari <mariamalzaatari@gmail.com>
2022-01-27 18:25:10 +0200

    Some Comments Added and remove js_solution since I only used jquery_solution

--- jquery_solution.js
+++ jquery_solution.js
@@ -3,30 +3,46 @@ var color_pattern = [];
 var level = 0;
 var user_click_counter = 0;
 
+// Start Game Section: Start Game by increasing level and binding/unbinding keypress event
+// -------------------------------
 $(document).keypress(function(){
     startGame()
 })
+
 function startGame(){
     game_start = true;
     increaseLevel();
     $(document).unbind("keypress");
 }
+// -------------------------------
+
+// Lose Game Section
+// -------------------------------
 function loseGame(){
+    //Change Heading and Screen Color, and Play Audio
     $("#title").text("Game Over, Press Any Key to Restart");
+    
     $("body").css("background-color", "red");
     setTimeout(function () {
         $("body").css("background-color", "#68074f");
     }, 100)
+    
     var audio = new Audio("sounds/wrong.mp3");
     audio.play();
 }
+// -------------------------------
+
+//Restart Game Section
+// -------------------------------
 function restart() {
+    //Restart all values and rebind keypress function
     game_start = false;
     color_pattern = [];
     level = 0;
     user_click_counter = 0;
     $(document).bind("keypress", startGame);
 }
+// -------------------------------
 function playColorSound(color) {
     var audio_src = "sounds/" + color + ".mp3";
     var audio = new Audio(audio_src);
